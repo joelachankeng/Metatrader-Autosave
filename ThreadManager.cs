af5daf93d214a45bb5cd3514666980125e707bf8
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2023-01-01 06:49:39 -0500

    create class to manage threads

---
+++ ThreadManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Metatrader_Autosaver
+{
+    class ThreadManager
+    {
+        private List<Thread> threadsList;
+
+        public ThreadManager()
+        {
+            threadsList = new List<Thread>();
+        }
+
+        public bool IsAnyThreadRunning() {
+            foreach (Thread threadItem in threadsList)
+            {
+                if (threadItem.IsAlive) return true;
+            }
+            return false;
+        }
+
+        public void CreateThread(Action threadFunction)
+        {
+            if (IsAnyThreadRunning()) return;
+            Thread backgroundThread = new Thread(() => threadFunction());
+            threadsList.Add(backgroundThread);
+
+            backgroundThread.Start();
+        }
+
+        public void KillAll()
+        {
+            foreach (Thread threadItem in threadsList)
+            {
+                if (threadItem.IsAlive) threadItem.Abort();
+            }
+        }
+
+       
+    }
+}
