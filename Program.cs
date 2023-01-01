0430046ed4fbf223189878bab1524efae5111a8b
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2023-01-01 06:17:34 -0500

    detect when user switches application

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Automation;
 
 namespace Metatrader_Autosaver
@@ -7,7 +8,36 @@ namespace Metatrader_Autosaver
     {
         static void Main(string[] args)
         {
+            Console.Title = "Metatrader Autosaver";
             Console.WriteLine("Metatrader Autosaver Initialized");
+
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                Console.WriteLine("Metatrader Autosaver already running. Only one instance of this application is allowed");
+                return;
+            }
+
+            AutomationFocusChangedEventHandler focusHandler = OnFocusChanged;
+            Automation.AddAutomationFocusChangedEventHandler(focusHandler);
+
+            Console.Read();
+
         }
+
+
+        static void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            AutomationElement focusedElement = sender as AutomationElement;
+            if (focusedElement != null)
+            {
+                int processId = focusedElement.Current.ProcessId;
+                using (Process process = Process.GetProcessById(processId))
+                {
+                    Console.WriteLine("Focusing on " + process.ProcessName);
+                }
+            }
+        }
+
+
     }
 }
