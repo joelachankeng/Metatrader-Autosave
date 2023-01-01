214ed4d21badbb969046ca9c8a9d01a67a70ffcc
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2023-01-01 05:49:28 -0500

    create new console c# project

---
+++ Metatrader-Autosaver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metatrader_Autosaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
